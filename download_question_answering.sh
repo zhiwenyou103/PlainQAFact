81caf0e6918c5f71f929e2a2a7781d35174a3161
Zhiwen You <60921570+zhiwenyou103@users.noreply.github.com>
2025-02-25 00:26:08 -0600

    Add files via upload

---
+++ download_question_answering.sh
@@ -0,0 +1,7 @@
+# Download the answering model
+mkdir models/answering
+cd models/answering
+gdown https://drive.google.com/uc?id=1q2Z3FPP9AYNz0RJKHMlaweNhmLQoyPA8
+unzip model.zip
+rm model.zip
+cd ../..
