3bcd2ce63d5c47b13d5f09ef59c989bac41d7a21
Zhiwen You <60921570+zhiwenyou103@users.noreply.github.com>
2025-02-25 20:59:25 -0600

    Update download_models.sh

--- download_models.sh
+++ download_models.sh
@@ -9,7 +9,7 @@ cd ../..
 # Download the classifier 
 mkdir models/classification
 cd models/classification
-gdown https://drive.google.com/file/d/1PuQA6bYsnKrIUU1i3ioKhr7xxWnJhdSh/view?usp=drive_link
+gdown https://drive.google.com/uc?id=1PuQA6bYsnKrIUU1i3ioKhr7xxWnJhdSh
 unzip learned_classifier.zip
 rm learned_classifier.zip
 cd ../..
